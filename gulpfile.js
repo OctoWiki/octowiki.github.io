bcbcaed45a0cbd0ee46202dbc7d106102b168b01
danielo515 <rdanielo@gmail.com>
2017-04-09 18:45:05 +0200

    - Gulpfile fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@
 
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('sass', function () {
-    gulp.src('./OctoWikiwiki/plugins/OctoWiki/styles/*.scss')
+gulp.task('sass', () => {
+    gulp.src('./wiki/plugins/OctoWiki/styles/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./OctoWikiwiki/plugins/OctoWiki/styles/'));
+        .pipe(gulp.dest('./wiki/plugins/OctoWiki/styles/'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./OctoWikiwiki/plugins/OctoWiki/styles/*.scss', ['sass']);
+gulp.task('sass:watch', () => {
+    gulp.watch('./wiki/plugins/OctoWiki/styles/*.scss', ['sass']);
 });
